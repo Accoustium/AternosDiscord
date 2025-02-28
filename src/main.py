15496ed5ef51f673596e3e21cd68d05791339cc2
Tim Pogue <t.pogue.python@gmail.com>
2025-02-28 17:18:44 +0000

    Add dotenv and pull status of server.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,14 @@
 from python_aternos import Client
+from dotenv import load_dotenv
+from os import getenv
+
+atclient = Client()
+load_dotenv("./src/.env")
+
+# Log in
+atclient.login(getenv("USERNAME"), getenv("PASSWORD"))
+aternos = atclient.account
+
+servers = aternos.list_servers()
+
+print(servers[0].status)
