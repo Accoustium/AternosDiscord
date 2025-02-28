8c3760ee3b6e039d761f2bf045817151f9725513
Tim Pogue <t.pogue.python@gmail.com>
2025-02-28 18:17:55 +0000

    Add some Discord stuff

--- src/main.py
+++ src/main.py
@@ -12,3 +12,15 @@ aternos = atclient.account
 servers = aternos.list_servers()
 
 print(servers[0].status)
+
+class DiscordBot(discord.Client):
+  async def on_ready(self):
+    print(f'Logged on as {self.user}!')
+
+  async def on_message(self, message):
+    print(f'Message from {message.author}: {message.content}')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = DiscordBot(intents=intents)
+client.run(getenv("DISCORD_TOKEN"))
